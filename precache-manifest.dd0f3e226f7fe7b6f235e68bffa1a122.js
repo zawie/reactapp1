1e985972428f02e2114bf98fc40adf99f1a98feb
Adam Zawierucha <zawiedev@gmail.com>
2019-09-21 15:00:39 -0500

    Updates

---
+++ precache-manifest.dd0f3e226f7fe7b6f235e68bffa1a122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad9734b384148012db558e09c312a40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc30ca91b3699feda905",
+    "url": "/static/css/main.86e06689.chunk.css"
+  },
+  {
+    "revision": "024febfa865aaaae4e74",
+    "url": "/static/js/2.53c7ab7f.chunk.js"
+  },
+  {
+    "revision": "cc30ca91b3699feda905",
+    "url": "/static/js/main.63c8131f.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  },
+  {
+    "revision": "fcc0055566d495fb3f4d5e57ee44e238",
+    "url": "/static/media/face.fcc00555.jpg"
+  },
+  {
+    "revision": "4c4001486728187aa8f7cffccc726365",
+    "url": "/static/media/resume.4c400148.pdf"
+  }
+]);
