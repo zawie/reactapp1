7aec7861414f96b561b724dfd5b0bad1fe9fb51b
Adam Zawierucha <zawiedev@gmail.com>
2019-09-21 14:41:48 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import face from './face.jpg';
+import resume from './resume.pdf';
 import './App.css';
 
+function random255(){
+  return Math.floor((Math.random() * 255))
+}
+
 function App() {
+  const [color, setColor] = useState("rgb(0,0,0)")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" style = {{ backgroundColor: color }} >
+        <img src={face} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Adam Zawierucha
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer" 
+        >
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href = {resume} 
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Resume
         </a>
+        <button onClick = { ()=>{ setColor("rgb(255,255,255)") }} > Change Color! </button>
       </header>
     </div>
   );
